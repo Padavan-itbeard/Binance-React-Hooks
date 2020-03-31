e09fb3827f172ab7247d5deab204cacdd593e6cb
Aleksandr Ivanov <aleksand7@yandex.ru>
2020-03-31 10:42:37 +0300

    Updates

---
+++ precache-manifest.75081ff8c41538c778deea5cc84ab8c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa0385fe6987bcbff1087908041ff99",
+    "url": "/Binance-React-Hooks/index.html"
+  },
+  {
+    "revision": "f9a34495d9ce46da3035",
+    "url": "/Binance-React-Hooks/static/css/main.f92d9bf8.chunk.css"
+  },
+  {
+    "revision": "e061dc3a786651be3220",
+    "url": "/Binance-React-Hooks/static/js/2.9462f683.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Binance-React-Hooks/static/js/2.9462f683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a34495d9ce46da3035",
+    "url": "/Binance-React-Hooks/static/js/main.9e5bae69.chunk.js"
+  },
+  {
+    "revision": "05d32ac0f55d643f3655",
+    "url": "/Binance-React-Hooks/static/js/runtime-main.11996545.js"
+  }
+]);
